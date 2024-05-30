74465dbefd362436f8f3424d6e3a3c136e415a18
KhadafiaRani <131012758+AizukiChan@users.noreply.github.com>
2024-05-31 04:37:16 +0900

    support.js

--- basicCommands/support.js
+++ basicCommands/support.js
@@ -4,10 +4,10 @@ module.exports = {
     name: 'support',
     description: 'support server of this Bot',
     execute(message, args) {
-        const supportServerLink = "https://discord.gg/xQF9f9yUEM";
-      const githubLink = "https://github.com/GlaceYT";
-      const replitLink = "https://replit.com/@GlaceYT";
-      const youtubeLink = "https://www.youtube.com/@GlaceYT";
+        const supportServerLink = "https://discord.gg/uY6zSGkwu6";
+      const githubLink = "https://discord.com/users/525515055857532948";
+      const replitLink = "https://web.facebook.com/shirayukichan21";
+      const youtubeLink = "https://www.tiktok.com/@khadafiarani";
         const embed = new EmbedBuilder()
             .setColor('#b300ff')
             .setAuthor({
@@ -15,8 +15,8 @@ module.exports = {
               iconURL: 'https://cdn.discordapp.com/attachments/1230824451990622299/1230824519220985896/6280-2.gif?ex=6638ae28&is=66375ca8&hm=13e4a1b91a95b2934a39de1876e66c11711c7b30ac1a91c2a158f2f2ed1c2fc6&', 
               url: 'https://discord.gg/xQF9f9yUEM'
           })
-            .setDescription(`➡️ **Join our Discord server for support and updates:**\n- Discord - ${supportServerLink}\n\n➡️ **Follow us on:**\n- GitHub - ${githubLink}\n- Replit - ${replitLink}\n- YouTube - ${youtubeLink}`)
-            .setImage('https://cdn.discordapp.com/attachments/1113800537402527903/1236803979996958740/11.png?ex=663956f7&is=66380577&hm=3b3c19a11adcb979517a133f2907f671305d23f1f5092cf7df043e6d5cab07bc&')
+            .setDescription(`➡️ **Join our Discord server for support and updates:**\n- Discord Server - ${supportServerLink}\n\n➡️ **Follow us on:**\n- Discord - ${githubLink}\n- Tiktok - ${replitLink}\n- Facebook - ${youtubeLink}`)
+            .setImage('https://cdn.discordapp.com/attachments/1223224428477943838/1245811286017708073/Blue__Purple_Anime_Aesthetic_Nightcore_Playlist_Music_Youtube_Thumbnail_.png?ex=665a1bac&is=6658ca2c&hm=d0745051fe8e4d0fa5cd4e73ca7620fd5fd75fc17e111e13dd2c5c6da3bce1c0&')
             .setTimestamp();
 
         message.reply({ embeds: [embed] });
